ff49a9df5248288c134a7ee27431e1c0afd60dd6
carlospinellowork <carlos.pinello@outlook.com>
2026-01-09 09:34:59 -0300

    chore: init next app with base structure

---
+++ app/admin/feature-flags/page.tsx
@@ -0,0 +1,8 @@
+export default function FeatureFlagsPage() {
+    return (
+        <div>
+            <h1>Feature Flags</h1>
+            <p>Administração de feature flags</p>
+        </div>
+    )
+}
